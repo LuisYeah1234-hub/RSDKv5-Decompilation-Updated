bb69a5ccf99e23c4b2e073a20b0faa4cad7ce800
LuisYeah1234-hub <64372171+LuisYeah1234-hub@users.noreply.github.com>
2025-05-15 11:43:11 +0200

    Update main.cpp

--- RSDKv5/main.cpp
+++ RSDKv5/main.cpp
@@ -5,7 +5,7 @@
 #define LinkGameLogic RSDK::LinkGameLogic
 #else
 #define EngineInfo RSDK::EngineInfo
-#include <GameMain.h>
+#include <../Game/GameMain.h>
 #define LinkGameLogic LinkGameLogicDLL
 #endif
 
