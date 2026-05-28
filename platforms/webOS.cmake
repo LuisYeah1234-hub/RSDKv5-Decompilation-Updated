829f598f4208fe9d3a62fc6bd49d68d397935185
LuisYeah1234-hub <64372171+LuisYeah1234-hub@users.noreply.github.com>
2026-05-28 19:11:09 +0200

    Now working webOS CMake build Port

--- platforms/webOS.cmake
+++ platforms/webOS.cmake
@@ -1,8 +1,23 @@
+# webOS deps
+set(DEP_PATH webOS)
 add_executable(RetroEngine ${RETRO_FILES})
 set(RETRO_SUBSYSTEM "SDL2" CACHE STRING "The subsystem to use")
-set(RETRO_MOD_LOADER OFF CACHE BOOL "Disable the mod loader" FORCE)
+if(NOT DEFINED ENV{STAGING_DIR})
+    message(FATAL_ERROR
+        "Can't find buildroot based toolchain. "
+        "Please ensure STAGING_DIR is set correctly."
+    )
+endif()
 set(STAGING_DIR $ENV{STAGING_DIR})
-target_link_libraries(RetroEngine PRIVATE dl pthread)
+
+# Enable built-in source compilation
+set(COMPILE_OGG ON)
+set(COMPILE_THEORA ON)
+
+# Optional compile flags for these libs
+set(OGG_FLAGS "")
+set(THEORA_FLAGS "")
+
 set(CMAKE_C_COMPILER arm-webos-linux-gnueabi-gcc)
 set(CMAKE_CXX_COMPILER arm-webos-linux-gnueabi-g++)
 set(CMAKE_FIND_ROOT_PATH $ENV{STAGING_DIR})
@@ -11,18 +26,34 @@ set_target_properties(RetroEngine PROPERTIES
     RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin
 )
 if(RETRO_SUBSYSTEM STREQUAL "SDL2")
-    set(SDL2_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/dependencies/webOS/SDL2/include/SDL2")
-    set(SDL2_LIB_DIR "${CMAKE_CURRENT_SOURCE_DIR}/dependencies/webOS/SDL2/lib")
+    set(SDL2_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/dependencies/${DEP_PATH}/SDL2/include/SDL2")
+    set(SDL2_LIB_DIR "${CMAKE_CURRENT_SOURCE_DIR}/dependencies/${DEP_PATH}/SDL2/lib")
     target_include_directories(RetroEngine PRIVATE ${SDL2_INCLUDE_DIR})
     target_link_directories(RetroEngine PRIVATE ${SDL2_LIB_DIR})
-    target_link_libraries(RetroEngine PRIVATE SDL2)
+    target_link_libraries(RetroEngine SDL2)
 else()
     message(FATAL_ERROR "RETRO_SUBSYSTEM must be set to SDL2")
 endif()
 
-
+target_link_libraries(RetroEngine	
+    ogg
+    dl
+    pthread
+)
 target_compile_definitions(RetroEngine PRIVATE ${SHARED_DEFINES} __webos__=ON)
-target_link_options(RetroEngine PRIVATE -Wl,-rpath=\$$ORIGIN/lib)
+set_target_properties(RetroEngine PROPERTIES
+    BUILD_RPATH "\$ORIGIN/lib"
+    INSTALL_RPATH "\$ORIGIN/lib"
+)
+
+if(WITH_RSDK AND NOT GAME_STATIC AND TARGET ${GAME_NAME})
+    add_custom_command(TARGET RetroEngine POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            $<TARGET_FILE:${GAME_NAME}>
+            $<TARGET_FILE_DIR:RetroEngine>/Game.so
+        VERBATIM
+    )
+endif()
 
 add_custom_command(TARGET RetroEngine POST_BUILD
     COMMAND OUTPUT_FILE=$<TARGET_FILE_DIR:RetroEngine>/gamecontrollerdb.txt sh ${CMAKE_CURRENT_SOURCE_DIR}/webos/gen_gamecontrollerdb.sh
